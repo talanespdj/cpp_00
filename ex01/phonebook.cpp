d3ce4b5b7620b2e975aa0502057abf3eb89dde2d
talanespdj <talanlipanda@gmail.com>
2024-12-13 01:39:07 +0100

    nouvel arrivage phonebook c'est gate mama

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tespandj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 01:15:47 by tespandj          #+#    #+#             */
+/*   Updated: 2024/12/13 01:38:13 by tespandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+#include <iostream>
+#include <stdarg.h>
+
+int	main(void)
+{
+	char	*in = NULL;
+
+	while (1)
+	{
+//		getline(
+		if (in && !strcmp(in, "EXIT"))
+			break;
+		break;
+	}
+	return (0);
+}
