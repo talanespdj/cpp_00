5f3505a887634e4e786169780675a9daf8a5d813
talanespdj <talanlipanda@gmail.com>
2024-12-16 01:45:52 +0100

    Diafarou a paye un uber

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -9,20 +9,31 @@
 /*   Updated: 2024/12/13 01:38:13 by tespandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <string.h>
-#include <iostream>
-#include <stdarg.h>
+#include "phonebook.hpp"
+#include <sstream>
 
 int	main(void)
 {
-	char	*in = NULL;
+	char	**in = NULL;
+	std::string	temp;
 
-	while (1)
-	{
-//		getline(
-		if (in && !strcmp(in, "EXIT"))
-			break;
-		break;
-	}
+	std::getline(std::cin, temp);
+
+	std::cout << temp << std::endl;
+		
+	// {
+
+	// }
+	std::cout << 22 << std::endl;
+	// while (!(strcmp(, "EXIT")) // tant qu l'input n'est pas egal a "EXIT"
+	// {
+	// 	if (!(strcmp(in, "ADD"))
+	// 		add()
+	// 	if (!(strcmp(in, "SEARCH"))
+	// 	else
+	// }
+	std::cout << "Bien le bonsoir" << std::endl;
+	std::cout << "Trou du cul" << std::endl;
+	(void)in;
 	return (0);
 }
---
+++ ex01/phonebook.hpp
@@ -0,0 +1,26 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include <string.h>
+#include <iostream>
+#include <stdarg.h>
+
+// class phonebook
+// {
+// 	private:
+// 		/* data */
+// 	public:
+// 		phonebook(/* args */);
+// 		~phonebook();
+// };
+
+// phonebook::phonebook(/* args */)
+// {
+// }
+
+// phonebook::~phonebook()
+// {
+// }
+
+
+#endif
